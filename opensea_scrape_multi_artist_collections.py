49a52d0ade7294b50afdb7f12957c362cab5c918
aprosdev <hitech.proton@gmail.com>
2023-06-08 11:58:36 -0700

    modify for adding the target

--- opensea_scrape_multi_artist_collections.py
+++ opensea_scrape_multi_artist_collections.py
@@ -29,8 +29,50 @@ with open(slugs_file, newline='') as csvfile:
     for row in reader:
         slugs.append(row)
 
-# slug = slugs[11111]
+# slug = slugs[165]
+# for slug in slugs:
+# url = collection_url.format(slug[0])
+# response = requests.get(url, headers=headers)
+# if response.status_code == 200:
+#     collection_info = response.json()
+
+#     if "primary_asset_contracts" in collection_info['collection']:
+#         # collection = collection_info['collection']["primary_asset_contracts"]
+#         collection = collection_info['collection']["editors"]
+#     else:
+#         print("There is no primary asset contract in this collection. Skipping...")
+    
+#     artist_set = set()
+#     if len(collection) > 1:
+#         print(f"The '{slug[0]}' collection has {len(collection)} different artists).")
+#         # artist_set.add(slug[0])
+#         with open('target.csv', mode='a', newline='') as file:
+#             writer = csv.writer(file)
+#             writer.writerow(slug[0])
+#     else:
+#         print(f"The '{slug[0]}' collection has only one artist.")
+    # if "created_by" in collection:
+    #     created_by = collection["created_by"]
+    #     for artist in created_by:
+    #         artist_set.add(artist["user"]["username"])
+    #     if len(artist_set) > 1:
+    #         print(f"The '{slug[0]}' collection has {len(artist_set)} different artists contributing to its assets.")
+    #     elif len(artist_set) == 1:
+    #         print(f"The '{slug[0]}' collection has only one artist.")
+    #     else:
+    #         print(f"///////No artist information found for the '{slug[0]}' collection.///////")
+    # else:
+    #     print(f"No artist information found for the '{slug[0]}' collection.")
+# else:
+#     print(f"Error requesting {url}. Status code: {response.status_code}")
+
 for slug in slugs:
+    print(slug[0])
+    i +=1
+    if i % 50 == 0:
+        print("------------------- 3 Seconds Delaying------------------")
+        time.sleep(3)
+
     url = collection_url.format(slug[0])
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
@@ -45,55 +87,9 @@ for slug in slugs:
         artist_set = set()
         if len(collection) > 1:
             print(f"The '{slug[0]}' collection has {len(collection)} different artists).")
-            artist_set.add(slug[0])
-            exit()
+            # artist_set.add(slug[0])
+            with open('target.csv', mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(slug[0])
         else:
             print(f"The '{slug[0]}' collection has only one artist.")
-    # if "created_by" in collection:
-    #     created_by = collection["created_by"]
-    #     for artist in created_by:
-    #         artist_set.add(artist["user"]["username"])
-    #     if len(artist_set) > 1:
-    #         print(f"The '{slug[0]}' collection has {len(artist_set)} different artists contributing to its assets.")
-    #     elif len(artist_set) == 1:
-    #         print(f"The '{slug[0]}' collection has only one artist.")
-    #     else:
-    #         print(f"///////No artist information found for the '{slug[0]}' collection.///////")
-    # else:
-    #     print(f"No artist information found for the '{slug[0]}' collection.")
-else:
-    print(f"Error requesting {url}. Status code: {response.status_code}")
-
-# for slug in slugs:
-    # print(slug[0])
-    # i +=1
-    # if i % 5 == 0:
-    #     print("------------------- 3 Seconds Delaying------------------")
-    #     time.sleep(3)
-
-    # url = collection_url.format(slug[0])
-    # response = requests.get(url, headers=headers)
-    # if response.status_code == 200:
-    #     collection_info = response.json()
-
-    #     if "primary_asset_contracts" in collection_info['collection']:
-    #         collection = collection_info['collection']["primary_asset_contracts"]
-    #     else:
-    #         print("There is no primary asset contract in this collection. Skipping...")
-        
-    #     artist_set = set()
-    #     if "created_by" in collection:
-    #         created_by = collection["created_by"]
-    #         for artist in created_by:
-    #             artist_set.add(artist["user"]["username"])
-    #     else:
-    #         print("There is no created_by in this collection. Skipping...")
-
-    #     if len(artist_set) > 1:
-    #         print(f"The '{slug[0]}' collection has {len(artist_set)} different artists contributing to its assets.")
-    #     elif len(artist_set) == 1:
-    #         print(f"The '{slug[0]}' collection has only one artist.")
-    #     else:
-    #         print(f"No artist information found for the '{slug[0]}' collection.")
-    # else:
-    #     print(f"Error requesting {url}. Status code: {response.status_code}")
