1cff733301a13cb7e4264ce0cbbe20b817e58949
aprosdev <hitech.proton@gmail.com>
2023-06-22 15:42:12 -0700

    modify get artists

--- get_artists.py
+++ get_artists.py
@@ -7,10 +7,11 @@ import time
 api_url = 'https://api.opensea.io/api/v1/assets?order_by={}&order_direction={}&offset={}&limit={}'
 headers = {'Accept': 'application/json', 'X-API-KEY': '609c816a67654d2fa8413c364469a8c1'}
 
-offset = 0
+offset = 10000
 limit = 50
 all_assets_count = 0
 artist_list = []
+total_scrapped_count = 0
 
 output_file = 'opensea_artists_new.csv'
 
@@ -21,6 +22,7 @@ while True:
 
         if response.status_code != 200:
             print('Request failed with status code', response.status_code)
+            print('30 seconds delaying...')
             time.sleep(30)
             continue
         else:
@@ -35,19 +37,19 @@ while True:
 
     new_assets = response.json()['assets']
     all_assets_count += len(new_assets)
+    print(f"Totally {all_assets_count} items scrapped...")
 
     for asset in new_assets:
         if asset['creator'] is None or asset['creator']['user'] is None or asset['creator']['user']['username']  is None:
             continue
         artist_name = asset['creator']['user']['username']
         collection_name = asset['collection']['name']
+
         if [artist_name, collection_name] not in asset_list:
             asset_list.append([artist_name, collection_name])
         else:
             continue
     
-    # unique_asset_list = list(set(asset_list))
-
     for asset in asset_list:
         with open(output_file, mode='a', newline='') as file:
             writer = csv.writer(file)
